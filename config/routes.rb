26c5fa653956b33dba563069eb71f9b530b70d0f
Travis Exline <t.exline22@gmail.com>
2019-07-30 18:34:24 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   resources :authors, only: [:show, :index] do
     resources :posts, only: [:show, :index, :new]
   end
+
+  resources :posts, only: [:show, :index, :new]
 end
