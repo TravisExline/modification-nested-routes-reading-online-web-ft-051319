9fddced825453e7cbf797281a77d8d1c6062dc36
Travis Exline <t.exline22@gmail.com>
2019-07-30 19:03:22 +0000

    Automatically backed up by Learn

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -2,8 +2,8 @@ module PostsHelper
   def author_id_field(post)
     if @post.author.nil?
       f.collection_select :author_id, Author.all, :id, :name
-    else  
+    else
       hidden_field_tag "post[author_id]", post.author_id
-    end  
+    end
   end
 end
